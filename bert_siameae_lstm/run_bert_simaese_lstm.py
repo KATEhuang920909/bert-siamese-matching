48498ba9c3f3e3a408f8a3bf48d83af1a51931b0
huangkai <18707125049@163.com>
2020-07-12 11:49:47 +0800

    修改contrastive loss ,simaese-bert模型的output，清理掉无用参数及代码。

--- bert_siameae_lstm/run_bert_simaese_lstm.py
+++ bert_siameae_lstm/run_bert_simaese_lstm.py
@@ -482,7 +482,7 @@ def model_fn_builder(bert_config, num_labels, init_checkpoint, learning_rate,
             show_dict = {
                 "total_loss": total_loss,
             }
-            logging_hook = tf.train.LoggingTensorHook(show_dict, every_n_iter=100)
+            logging_hook = tf.train.LoggingTensorHook(show_dict, every_n_iter=10)
             #logging_hook = tf.train.LoggingTensorHook({"total_loss:": total_loss}, every_n_iter=10)
             output_spec = tf.contrib.tpu.TPUEstimatorSpec(
                 mode=mode,
